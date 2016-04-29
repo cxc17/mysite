7de7d00df282c90bc21193872ae0780d54abbb35
taveen_windows <caixiaochuan@sina.cn>
2016-04-29 15:45:54 +0800

    taveen 4/29

--- mysite/test.cpp
+++ mysite/test.cpp
@@ -2,31 +2,66 @@
 using namespace std;
 
 
-int compete( int (*a)  [2])
+void compete(int** a, int low, int high, int num)
 {	
-	cout<<a[0][0];
-	return 0;	
-} 
-
+	if (num == 2)
+	{
+		a[low][low+1] = 1;
+		a[low+1][low] = 1;
+		
+		return;
+	}
+	else if (num == 3)
+	{
+		a[low][low+1] = 1;
+		a[low+1][low] = 1;
+		a[low][low+2] = 2;
+		a[low+2][low] = 2;
+		a[low+1][low+2] = 3;
+		a[low+2][low+1] = 3;
+		
+		return;
+	}
+	
+	int num1 = num / 2;
+	int num2 = num -num1;
+	
+	compete(a, low, low+num1-1, num1);
+	compete(a, low+num1, high, num2);
+	
+	for 
+	
+	return;
+}
 
 
 int main()
 {
 	int number;
+	int **p;
 	
 	cout<<"input number:"; 
 	cin>>number;
+	number = number+1;
 	
-	int a[number+1][number+1] = {0};
+	p = new int*[number];
+	p[0] = new int[number*number];
 	
-	for (int i=1; i<=number; i++)
+	for (int i=1; i<number; i++)
+		p[i] = p[0] + number*i;
+		
+	for (int i=1; i<number; i++)
+		for (int j=1; j<number; j++)
+			p[i][j] = 0;
+	
+	compete(p, 1, number-1, number-1);
+	
+	for (int i=1; i<number; i++)
 	{
-		for (int j=1; j<=number; j++)
-			cout<<a[i][j]<<" ";
-		cout<<"\n";
+		for (int j=1; j<number; j++)
+			cout<<p[i][j]<<" ";
+		cout<<endl;
 	}
-	int b[2][2]={1,2,3,4};
 	
-	compete(b);
-	
+	return 0;
 }
