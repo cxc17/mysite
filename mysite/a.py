32513efba4d5dafadacd20380c99719547458dfb
caixiaochuan <caixiaochuan@sina.cn>
2016-06-01 00:14:39 +0800

    home 5/31

--- mysite/a.py
+++ mysite/a.py
@@ -1,26 +1,59 @@
-# coding:utf-8
+# # coding:utf-8
 
-import multiprocessing
-import urllib
-
-def run(i):
-    print i
-    urllib.urlopen('http://www.baidu.com')
-    i = str(i) + '\n'
-    f = open('1.txt', 'a+')
-    f.write(i)
-    f.close()
-    print i,'ok'
+# from multiprocessing import Lock, Pool
+# import urllib
+# import time
 
 
-if __name__ == '__main__':
-    f = open('1.txt', 'w')
-    f.close()
-    pool = multiprocessing.Pool(processes=5)
-    for i in xrange(10000):
-        pool.apply_async(run, (i+1, ))
-        # run(i)
-    print 'asd'
+# def run(i, lock):
+#     # print i
+#     # urllib.urlopen('https://www.baidu.com')
+#     # # time.sleep(0.1)
+#     # i = str(i) + '\n'
+#     # f = open('1.txt', 'a+')
+#     # f.write(i)
+#     # # print time.time()
+#     # f.close()
+#     # # print i,'ok'
+#     print 123
+#     lock.acquire()
+#     print i
+#     time.sleep(3)
+#     lock.release()
 
-    pool.close()
-    pool.join()
+
+# if __name__ == '__main__':
+#     f = open('1.txt', 'w')
+#     f.close()
+#     pool = Pool(processes=1)
+
+#     lock = Lock()
+#     for i in xrange(10):
+#         pool.apply_async(run, (i, lock, ))
+#         # run(i,lock)
+
+#     pool.close()
+#     pool.join()
+
+from multiprocessing import Process,Lock
+import time
+
+def f(l,i):
+
+      # l.acquire()
+
+      print "hello world",i
+      time.sleep(1)
+      # l.release()
+
+ 
+
+if __name__ == "__main__":
+
+        lock = Lock()
+
+        for num in range(10):
+
+                Process(target=f,args=(lock,num)).start()
+
+
---
+++ mysite/b.py
@@ -0,0 +1,41 @@
+#coding=utf-8
+import threading
+import time
+import urllib
+
+
+def run(lock):
+    for j in xrange(1000):
+        # print i, j
+        urllib.urlopen('https://www.baidu.com')
+        while 1:
+            lock.acquire()
+            f = open('1.txt', 'a+')
+            f.write(str(j+1)+'\n')
+            f.close()
+            lock.release()
+            break
+
+
+if __name__ == '__main__':
+    threads = []
+    f = open('1.txt', 'w')
+    f.close()
+
+    a = time.time()
+
+    lock = threading.Lock()
+    run(lock)
+
+    # for i in xrange(10):
+    #     t1 = threading.Thread(target=run,args=(lock,))
+    #     threads.append(t1)
+
+    # for t in threads:
+    #     t.setDaemon(True)
+    #     t.start()
+
+    # t.join()
+    b = time.time()
+
+    print (b-a)
