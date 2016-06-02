0ab9f83d58145bd4898d6cb897ed3c14e11282e2
taveen_windows <caixiaochuan@sina.cn>
2016-06-02 17:23:49 +0800

    taveen 6/2

--- mysite/a.py
+++ mysite/a.py
@@ -1,43 +1,45 @@
 # coding:utf-8
 
-from multiprocessing import Lock, Pool
+from multiprocessing import Manager, Lock, Pool, Process
+import time, multiprocessing
 import urllib
-import time
 
 
-# a = 0
-def run(i):
-    for x in xrange(20000000):
-    # a = 1
+def run(lock):
+    for x in xrange(400000000):
         x += 1
     return
 
-    try:
-        urllib.urlopen('https://www.baidu.com')
-    except Exception, e:
-        print i
 
-    i = str(i) + '\n'
-    f = open('2.txt', 'a+')
-    f.write(i)
-    f.close()
-
+# IO 密集型任务
+def run2(lock):
+    for j in xrange(3):
+        try:
+            urllib.urlopen('https://www.baidu.com')
+        except Exception, e:
+            pass
+        # lock.acquire()
+        f = open('2.txt', 'a+')
+        f.write(str(j+1)+'\n')
+        f.close()
+        # lock.release()
 
 if __name__ == '__main__':
     f = open('2.txt', 'w')
     f.close()
-
+    # pool_size = multiprocessing.cpu_count()
+    # print pool_size
     c = time.time()
-    for x in xrange(20000000):
-    # a = 1
-        x += 1
-    # pool = Pool(processes=10)
 
-    # for i in xrange(10):
-    #     pool.apply_async(run, (i, ))
-
-    # pool.close()
-    # pool.join()
+    lock = Manager().Lock()
+    pool = Pool(processes=2)
+    for i in xrange(2):
+        pool.apply_async(run2, (lock,))
+        # p = Process(target=run2,args=(lock, ))
+        # p.start()
+        # p.join()
+    pool.close()
+    pool.join()
 
     b = time.time()
     print b - c
