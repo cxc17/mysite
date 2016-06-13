df4105ee0ac28602da75fde0bcdaaaf2d37b3947
caixiaochuan <caixiaochuan@sina.cn>
2016-06-14 00:12:41 +0800

    home 6/13

--- mysite/byrbbs/byrbbs/spiders/allspider.py
+++ mysite/byrbbs/byrbbs/spiders/allspider.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
 from scrapy import Spider
 from scrapy import FormRequest
 from scrapy import Request
@@ -15,13 +15,8 @@ class AllSpider(Spider):
     )
 
     def start_requests(self):
-        return [Request('https://bbs.byr.cn/user/ajax_login.json', meta={'cookiejar': 1}, callback=self.post_login,
-                        headers={'X-Requested-With': 'XMLHttpRequest'})]
-
-    def post_login(self, response):
-        print response.body
         return [FormRequest('https://bbs.byr.cn/user/ajax_login.json',
-                            meta={'cookiejar': response.meta['cookiejar']},
+                            meta={'cookiejar': 1},
                             formdata={'id': 'oneseven', 'passwd': '349458cddCXC'},
                             callback=self.logged_in,
                             headers={'X-Requested-With': 'XMLHttpRequest'})]
@@ -32,8 +27,8 @@ class AllSpider(Spider):
         if login_info['ajax_msg'] != u'操作成功':
             print 'ERROR!!!'
             return
-        print '+++++++++++++'
-        conn = MySQLdb.connect(host="192.168.1.28", user="root", passwd="123456", db="byr", charset="utf8")
+
+        conn = MySQLdb.connect(host="127.0.0.1", user="root", passwd="123456", db="byr", charset="utf8")
         cursor = conn.cursor()
 
         # 从数据库中找出每个版块的名称
@@ -42,31 +37,53 @@ class AllSpider(Spider):
         cursor.execute(sql)
         for board in cursor.fetchall():
             section_url = 'https://bbs.byr.cn/board/%s' % board[0]
-            return Request(section_url,
-                           meta={'board_name': board[0], 'cookiejar': response.cookiejar},
-                           callback=self.board_page)
+            yield Request(section_url,
+                          meta={'board_name': board[0], 'cookiejar': response.meta['cookiejar']},
+                          headers={'X-Requested-With': 'XMLHttpRequest'},
+                          callback=self.board_page)
 
         cursor.close()
         conn.close()
 
     def board_page(self, response):
         sel = Selector(response)
-        print response.body
-        return
+
         PostNum_xpath = '//div[4]/div[1]/ul/li[1]/i/text()'
-        post_num = sel.xpath(PostNum_xpath).extract()
-        print post_num
+        post_num = int(sel.xpath(PostNum_xpath).extract()[0])
 
+        if post_num % 30 == 0:
+            post_page = post_num / 30 + 1
+        else:
+            post_page = post_num / 30 + 2
 
+        for num in xrange(1, post_page):
+            page_url = 'https://bbs.byr.cn/board/%s?p=%s' % (response.meta['board_name'], num)
 
+            yield Request(page_url,
+                          meta={'board_name': response.meta['board_name'], 'cookiejar': response.meta['cookiejar']},
+                          headers={'X-Requested-With': 'XMLHttpRequest'},
+                          callback=self.post_list)
 
+    def post_list(self, response):
+        sel = Selector(response)
 
+        for i in xrange(1, 31):
+            PostURL_xpath = '//div[3]/table/tbody/tr[%s]/td[2]/a/@href' % i
 
+            try:
+                post_url = sel.xpath(PostURL_xpath).extract()[0]
+            except:
+                return
 
+            post_url = 'https://bbs.byr.cn' + post_url
 
+            yield Request(post_url,
+                          meta={'board_name': response.meta['board_name'], 'cookiejar': response.meta['cookiejar']},
+                          headers={'X-Requested-With': 'XMLHttpRequest'},
+                          callback=self.post_content)
 
-
-
+    def post_content(self, response):
+        pass
 
 
 
