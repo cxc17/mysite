216fd62d263fcf4611f32815632b86a962d647b3
caixiaochuan <caixiaochuan@sina.cn>
2016-07-18 00:36:36 +0800

    home 7/17

--- mysite/byrbbs/bbs_search.py
+++ mysite/byrbbs/bbs_search.py
@@ -1,104 +1,109 @@
 # coding: utf-8
 
+from byrbbs.SpiderConfig import SpiderConfig
+from byrbbs.mysqlclient import get_mysql
 
 from lxml import etree
 import requests
-import re
-import MySQLdb
-
-headers = {'X-Requested-With': 'XMLHttpRequest'}
-
-login_data = {
-    'id': 'oneseven',
-    'passwd': '349458cddCXC'
-}
-
-count = 1
-
-# 以发帖人的ID或标题中的词语为关键字查找帖子 
-def keyword_search(session, section_url, board_name):
-	global count
-
-	req = session.get(section_url, headers=headers)
-	try:
-		html = req.content.decode("gbk")
-	except Exception, e:
-		html = req.content
-	
-	sel = etree.HTML(html)
-
-	xpath_num = '//div[3]/div/ul/li[1]/i/text()'
-	title_num = int(sel.xpath(xpath_num)[0])
-
-	if title_num % 80 == 0:
-		page = title_num / 80 - 1
-	else:
-		page = title_num / 80 
 
 
-	for board_num in xrange(1,81):
-		try:
-			xpath_name = '//div[2]/table/tr[%s]/td[2]/a/text()' % board_num
-			xpath_url = '//div[2]/table/tr[%s]/td[2]/a/@href' % board_num
-			title = sel.xpath(xpath_name)[0]
-			url = 'https://bbs.byr.cn/' + sel.xpath(xpath_url)[0]
-		except Exception, e:
-			break
+class BBSsearch(object):
+    count = 1
 
-		print count, board_name
-		print title, url
-		count += 1
+    def __init__(self):
+        pass
 
+    # 以发帖人的ID或标题中的词语为关键字查找帖子
+    def keyword_search(self, session, section_url, board_name):
+        req = session.get(section_url, headers={'X-Requested-With': 'XMLHttpRequest'})
+        try:
+            html = req.content.decode("gbk")
+        except:
+            html = req.content
 
-	for i in xrange(page):
-		section_url = section_url + '&p=%s' % (i+2)
-		req = session.get(section_url, headers=headers)
-		try:
-			html = req.content.decode("gbk")
-		except Exception, e:
-			html = req.content
-		
-		sel = etree.HTML(html)
+        sel = etree.HTML(html)
 
-		for board_num in xrange(1,81):
-			try:
-				xpath_name = '//div[2]/table/tr[%s]/td[2]/a/text()' % board_num
-				xpath_url = '//div[2]/table/tr[%s]/td[2]/a/@href' % board_num
-				title = sel.xpath(xpath_name)[0]
-				url = 'https://bbs.byr.cn/' + sel.xpath(xpath_url)[0]
-			except Exception, e:
-				break
+        xpath_num = '//div[3]/div/ul/li[1]/i/text()'
+        title_num = int(sel.xpath(xpath_num)[0])
 
-			print count, board_name
-			print title, url
-			count += 1
+        if title_num % 80 == 0:
+            page = title_num / 80 - 1
+        else:
+            page = title_num / 80
 
+        for board_num in xrange(1, 81):
+            try:
+                xpath_name = '//div[2]/table/tr[%s]/td[2]/a/text()' % board_num
+                xpath_url = '//div[2]/table/tr[%s]/td[2]/a/@href' % board_num
+                title = sel.xpath(xpath_name)[0]
+                url = 'https://bbs.byr.cn/' + sel.xpath(xpath_url)[0]
+            except:
+                break
 
-def main():
-	session = requests.session()
+            print self.count, board_name
+            print title, url
+            self.count += 1
 
-	login_url = 'https://bbs.byr.cn/user/ajax_login.json'
-	session.post(login_url, data=login_data, headers=headers)
+        for i in xrange(page):
+            section_url += '&p=%s' % (i+2)
+            req = session.get(section_url, headers={'X-Requested-With': 'XMLHttpRequest'})
+            try:
+                html = req.content.decode("gbk")
+            except:
+                html = req.content
 
-	conn=MySQLdb.connect(host="192.168.1.98", user="root", passwd="123456", db="byr", charset="utf8")    
-	cursor = conn.cursor() 
+            sel = etree.HTML(html)
 
-	# 从数据库中找出每个版块的名称
-	sql = "select board_name from board"
-	
-	cursor.execute(sql)      
-	for row in cursor.fetchall():
-		# 以发帖人的ID为关键字查找帖子 
-		section_url = 'https://bbs.byr.cn/s/article?au=%s&b=%s' % ('nuanyangyang', row[0])
-		keyword_search(session, section_url, row[0])
+            for board_num in xrange(1, 81):
+                try:
+                    xpath_name = '//div[2]/table/tr[%s]/td[2]/a/text()' % board_num
+                    xpath_url = '//div[2]/table/tr[%s]/td[2]/a/@href' % board_num
+                    title = sel.xpath(xpath_name)[0]
+                    url = 'https://bbs.byr.cn/' + sel.xpath(xpath_url)[0]
+                except:
+                    break
 
-		# 以标题中的词语为关键字查找帖子 
-		# section_url = 'https://bbs.byr.cn/s/article?t1=%s&b=%s' % ('python', 'Python')#row[0])
-		# keyword_search(session, section_url, 'Python')#row[0])
+                print self.count, board_name
+                print title, url
+                self.count += 1
 
-	cursor.close()
-	conn.close()
+    def login(self, search_type, search_name):
+        session = requests.session()
 
+        login_url = 'https://bbs.byr.cn/user/ajax_login.json'
+        session.post(login_url, data={'id': SpiderConfig.account_id, 'passwd': SpiderConfig.account_passwd},
+                     headers={'X-Requested-With': 'XMLHttpRequest'})
+
+        # 从数据库中找出每个版块的名称
+        sql = "select board_name from board"
+        mh = get_mysql()
+        ret_sql = mh.select(sql)
+
+        for row in ret_sql:
+
+            if search_type == 'name':
+                # 以发帖人的ID为关键字查找帖子
+                section_url = 'https://bbs.byr.cn/s/article?au=%s&b=%s' % (search_name, row[0])
+                self.keyword_search(session, section_url, row[0])
+            elif search_type == 'title':
+                # 以标题中的词语为关键字查找帖子
+                section_url = 'https://bbs.byr.cn/s/article?t1=%s&b=%s' % (search_name, row[0])
+                self.keyword_search(session, section_url, row[0])
+            else:
+                print "search_type ERROR!!!"
+                return
+
+    def bbssearch(self, search_type=None, search_name=None):
+        if not search_type:
+            print "search_type None!!!"
+            return
+
+        if not search_name:
+            print "search_name None!!!"
+            return
+
+        SpiderConfig.initialize()
+        self.login(search_type, search_name)
 
 if __name__ == '__main__':
-	main()
+    BBSsearch().bbssearch('name', 'xymanman')
--- mysite/byrbbs/board_name.py
+++ mysite/byrbbs/board_name.py
@@ -1,63 +1,58 @@
 # coding: utf-8
 # 爬取所有版块的名称
 
+from byrbbs.SpiderConfig import SpiderConfig
+from byrbbs.mysqlclient import get_mysql
 
 from lxml import etree
 import requests
 import re
-import MySQLdb
-
-headers = {'X-Requested-With': 'XMLHttpRequest'}
-
-login_data = {
-    'id': 'oneseven',
-    'passwd': '349458cddCXC'
-}
 
 
-def run(session, section_url):
-	req = session.get(section_url, headers=headers)
-	html = req.content.decode("gbk")
-	sel = etree.HTML(html)
+class WriteBoard(object):
+    def __init__(self):
+        pass
 
-	conn=MySQLdb.connect(host="192.168.1.98", user="root", passwd="123456", db="byr", charset="utf8")    
-	cursor = conn.cursor() 
-	sql = "insert into board (`board_name`) values ('%s')"
+    def run(self, session, section_url):
+        req = session.get(section_url, headers={'X-Requested-With': 'XMLHttpRequest'})
+        html = req.content.decode("gbk")
+        sel = etree.HTML(html)
 
-	section_urls =[] 
-	for board_num in xrange(1,50):
-		try:
-			xpath_name = '//div[2]/table/tbody/tr[%s]/td[1]/a/@href' % board_num
-			board_url = sel.xpath(xpath_name)[0]
-		except Exception, e:
-			break
+        sql = "insert into board (`board_name`) values ('%s')"
 
-		if 'board' in board_url:
-			board_name = re.findall(r'board/(.+)', board_url)[0]
-			cursor.execute(sql % board_name)
-			print board_num, board_url
-		else:
-			section_url = 'https://bbs.byr.cn' + board_url
-			print board_num
-			run(session, section_url)
-			
-	cursor.close()
-	conn.commit()
-	conn.close()
+        for board_num in xrange(1, 50):
+            try:
+                xpath_name = '//div[2]/table/tbody/tr[%s]/td[1]/a/@href' % board_num
+                board_url = sel.xpath(xpath_name)[0]
+            except:
+                break
 
+            if 'board' in board_url:
+                board_name = re.findall(r'board/(.+)', board_url)[0]
+                mh = get_mysql()
+                mh.execute(sql % board_name)
+                print board_num, board_url
+            else:
+                section_url = 'https://bbs.byr.cn' + board_url
+                print board_num
+                self.run(session, section_url)
 
-def main():
-	session = requests.session()
+    def login(self):
+        session = requests.session()
 
-	login_url = 'https://bbs.byr.cn/user/ajax_login.json'
-	session.post(login_url, data=login_data, headers=headers)
+        login_url = 'https://bbs.byr.cn/user/ajax_login.json'
+        session.post(login_url, data={'id': SpiderConfig.account_id, 'passwd': SpiderConfig.account_passwd},
+                     headers={'X-Requested-With': 'XMLHttpRequest'})
 
-	for section_num in xrange(0, 10):
-		section_url = 'https://bbs.byr.cn/section/%s' % section_num
+        for section_num in xrange(0, 10):
+            section_url = 'https://bbs.byr.cn/section/%s' % section_num
 
-		run(session, section_url)
+            self.run(session, section_url)
+
+    def writeboard(self):
+        SpiderConfig.initialize()
+        self.login()
 
 
 if __name__ == '__main__':
-	main()
-	
+    WriteBoard().writeboard()
