571524c279fffebf907f0a54f49a6ead0b19f3af
caixiaochuan <caixiaochuan@sina.cn>
2016-08-25 00:26:27 +0800

    home 8/24

--- mysite/test.py
+++ mysite/test.py
@@ -1,38 +1,71 @@
 # coding: utf-8
- 
+
+import os
 import MySQLdb
 import ConfigParser
 
 
-# def Import_SQL():
-# 	# 配置文件路径
-# 	config_path = './sql.conf'
+def Import_SQL():
+	# 配置文件路径
+	config_path = './sql.conf'
 
-# 	config = ConfigParser.ConfigParser()
-# 	config.read(config_path)
+	config = ConfigParser.ConfigParser()
+	config.read(config_path)
 
-# 	# 数据库配置信息
-# 	host = config.get('database', 'host')
-# 	user = config.get('database', 'user')
-# 	passwd = config.get('database', 'passwd')
-# 	db = config.get('database', 'db')
+	# 数据库配置信息
+	host = config.get('database', 'host')
+	user = config.get('database', 'user')
+	passwd = config.get('database', 'passwd')
+	db = config.get('database', 'db')
 
-# 	# 连接数据库
-# 	conn = MySQLdb.connect(user=user, passwd=passwd, host=host, db=db, charset='utf8')
-# 	cur = conn.cursor()
+	# path = "/Users/caixiaochuan/Desktop/mysite/mysite/byr.sql"
 
-# 	cur.execute("source news.sql")
+	# os.system("mysql -h%s -u%s -p%s" %(host, user, passwd))
+	# os.system("use %s;" % db)
+	# os.system("source %s;" %path)
+	# os.system("exit;")
 
-# 	cur.close()
-# 	conn.close()
+	# return
+
+	# 连接数据库
+	conn = MySQLdb.connect(user=user, passwd=passwd, host=host, db=db, charset='utf8')
+	cur = conn.cursor()
 
 
-# if __name__ == '__main__':
-# 	Import_SQL()
+	# sql = open("/Users/caixiaochuan/Desktop/byrbbs/byr.sql").read()
+
+	sql = open("/Users/caixiaochuan/Desktop/byrbbs/byr.sql")
+	sql = "".join(sql.readlines()).strip()
+
+	for i in sql.split(";"):
+		if i:
+			cur = conn.cursor()
+			cur.execute(i)
+			cur.close()
+
+	# cur.execute("source /Users/caixiaochuan/Desktop/byrbbs/byr.sql")
+	# cur.execute(sql)
+
+	cur.close()
+	conn.close()
 
 
-import time 
+def Export_SQL():
+	# 配置文件路径
+	config_path = './sql.conf'
 
-a = {"1":1,"2":2}
+	config = ConfigParser.ConfigParser()
+	config.read(config_path)
 
-print len(a)
+	# 数据库配置信息
+	host = config.get('database', 'host')
+	user = config.get('database', 'user')
+	passwd = config.get('database', 'passwd')
+	db = config.get('database', 'db')
+
+	os.system(" mysqldump -h%s -u%s -p%s %s > byr.sql" %(host, user, passwd ,db))
+
+
+if __name__ == '__main__':
+	Import_SQL()
+	# Export_SQL()
