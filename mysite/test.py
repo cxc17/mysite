89990fb04e38aadccc4aaf17309de85906e02ac7
taveen_windows <caixiaochuan@sina.cn>
2017-06-13 16:47:48 +0800

    lab 6/13

--- mysite/test.py
+++ mysite/test.py
@@ -1,18 +1,38 @@
-import tornado.ioloop
-import tornado.web
+# import tornado.ioloop
+# import tornado.web
+#
+#
+# class MainHandler(tornado.web.RequestHandler):
+#     def get(self):
+#         self.write("Hello, world")
+#
+#
+# def make_app():
+#     return tornado.web.Application([
+#         (r"/", MainHandler),
+#     ])
+#
+# if __name__ == "__main__":
+#     app = make_app()
+#     app.listen(8888)
+#     tornado.ioloop.IOLoop.current().start()
+#
 
+import math
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello, world")
+while 1:
+    try:
+        l, r = map(int, raw_input().split())
+        cache = [0]*10
 
+        for i in xrange(1, r+1):
+            a = math.ceil(l*1.0/i)
+            b = r/i - a + 1
+            if b > 0:
+                cache[int(str(i)[0])] += int(b)
 
-def make_app():
-    return tornado.web.Application([
-        (r"/", MainHandler),
-    ])
+        for i in xrange(1, 10):
+            print cache[i]
+    except:
+        break
 
-if __name__ == "__main__":
-    app = make_app()
-    app.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
